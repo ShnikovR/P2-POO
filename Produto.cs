07e36cbd8a25ed3576adc0fd510ea2a3ec30e79d
ShnikovR <filibebreciane@gmail.com>
2025-06-05 17:40:53 -0300

    Código da classe produto e pedido
    Código da classe produto e pedido, métodos, atributos etc

---
+++ Produto.cs
@@ -0,0 +1,25 @@
+public class Produto
+{
+    private static int LocalizarID_Produto = 1;
+
+    public int Id { get; private set; }
+    public string Nome { get; private set; }
+    public decimal Preco { get; private set; }
+    public string Categoria { get; private set; }
+
+    // Construtor
+    public Produto(string nome, decimal preco, string categoria)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+            throw new ArgumentException("Nome inválido.");
+        if (preco <= 0)
+            throw new ArgumentException("Preço deve ser maior que zero.");
+        if (string.IsNullOrWhiteSpace(categoria))
+            throw new ArgumentException("Categoria inválida.");
+
+        Id = _contadorId++;
+        Nome = nome;
+        Preco = preco;
+        Categoria = categoria;
+    }
+}
